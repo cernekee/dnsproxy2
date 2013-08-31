eda95287dbc8e02c9d3eae5017a3e3380a6cce94
Kevin Cernekee <cernekee@gmail.com>
2013-08-30 22:25:39 -0700

    dnsproxy2: Create new socket and pass it into DnsProxyListener
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

--- jni/dnsproxy2.cpp
+++ jni/dnsproxy2.cpp
@@ -16,16 +16,60 @@
  */
 
 #include <errno.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/un.h>
+
 #include <cutils/log.h>
 
 #include "DnsProxyListener.h"
 
+#define SOCKPATH        "/dev/socket/dnsproxyd"
+
+static int restore_oldsock;
+
+static void die(const char *fmt, ...)
+{
+    va_list ap;
+
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
+    exit(1);
+}
+
 int main(int argc, char **argv)
 {
+    int sockfd;
+    struct sockaddr_un sock;
+    struct stat statbuf;
+    char buf[16];
     DnsProxyListener *dpl;
+
+    if (stat(SOCKPATH, &statbuf) == 0) {
+        unlink(SOCKPATH ".bak");
+        rename(SOCKPATH, SOCKPATH ".bak");
+        restore_oldsock = 1;
+    }
+
+    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        die("socket() failed: %s\n", strerror(errno));
+
+    memset(&sock, 0, sizeof(sock));
+    sock.sun_family = AF_UNIX;
+    strcpy(sock.sun_path, SOCKPATH);
+
+    if (bind(sockfd, (struct sockaddr *)&sock, sizeof(sock)) < 0)
+        die("can't bind: %s\n", strerror(errno));
+
+    sprintf(buf, "%d", sockfd);
+    setenv("ANDROID_SOCKET_dnsproxyd", buf, 1);
+    setenv("ANDROID_DNS_MODE", "local", 1);
 
     dpl = new DnsProxyListener();
     if (dpl->startListener()) {
