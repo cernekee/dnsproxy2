04b1173e6edb58186b18080858becbbf2ae154c8
Kevin Cernekee <cernekee@gmail.com>
2013-08-30 20:58:33 -0700

    Add a bunch of other deps from system/
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,10 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE	:= dnsproxy2
 LOCAL_SRC_FILES	:= dnsproxy2.cpp FrameworkListener.cpp \
-	FrameworkCommand.cpp DnsProxyListener.cpp
-LOCAL_CFLAGS	:= -DHAVE_SYS_UIO_H -include netdb-private.h
+	FrameworkCommand.cpp DnsProxyListener.cpp SocketListener.cpp \
+	SocketClient.cpp NetdCommand.cpp logd_write.c atomic.c
+LOCAL_CFLAGS	:= -DHAVE_SYS_UIO_H \
+	-DANDROID_SMP=1 \
+	-include netdb-private.h
 
 include $(BUILD_EXECUTABLE)
