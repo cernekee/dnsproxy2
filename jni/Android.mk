ebd317962b54800e444ee012792d6a890359d0f2
Kevin Cernekee <cernekee@gmail.com>
2013-08-30 20:41:15 -0700

    Rename dnsproxy2.c to dnsproxy2.cpp
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE	:= dnsproxy2
-LOCAL_SRC_FILES	:= dnsproxy2.c FrameworkListener.cpp \
+LOCAL_SRC_FILES	:= dnsproxy2.cpp FrameworkListener.cpp \
 	FrameworkCommand.cpp DnsProxyListener.cpp
 LOCAL_CFLAGS	:= -DHAVE_SYS_UIO_H -include netdb-private.h
 
