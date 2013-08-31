a4c9d28388201c520cc80ef98beb7e27a23c1991
Kevin Cernekee <cernekee@gmail.com>
2013-08-30 21:35:50 -0700

    dnsproxy2: Instantiate the proxy server on startup
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

--- jni/dnsproxy2.cpp
+++ jni/dnsproxy2.cpp
@@ -1,6 +1,42 @@
+/* vi:set sw=4 sts=4 expandtab: */
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <cutils/log.h>
+
+#include "DnsProxyListener.h"
 
 int main(int argc, char **argv)
 {
-	return 0;
+    DnsProxyListener *dpl;
+
+    dpl = new DnsProxyListener();
+    if (dpl->startListener()) {
+        ALOGE("Unable to start DnsProxyListener (%s)", strerror(errno));
+        exit(1);
+    }
+
+    // Eventually we'll become the monitoring thread
+    while(1) {
+        sleep(1000);
+    }
+
+    return 0;
 }
